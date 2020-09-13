44cf98bd875c94c0d2da503963aa83f7fa0ee0fe
Dasithz <dasith9b@gmail.com>
2020-09-13 10:59:28 -0400

    Initial Commit with Changes

---
+++ src/main/java/com/dk/stringprocessorservice/StringProcessorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dk.stringprocessorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringProcessorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StringProcessorServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dk/stringprocessorservice/StringProcessorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dk.stringprocessorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StringProcessorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
